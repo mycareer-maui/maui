ca8e6c8dca9a11cf0c9395d97c7b60b95551af8c
Apple <apple@Apples-MacBook-Air.local>
2025-10-27 20:32:48 +0530

    my commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int i = 10;
+
+            Console.Write("int" + i);
+        }
+    }
+}
